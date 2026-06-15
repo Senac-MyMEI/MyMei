6c811278e814ddb7fda8a0217c1e6ea2caa078de
Kiratsuji <132310994+Kiratsuji@users.noreply.github.com>
2026-06-15 11:34:45 -0300

    feat: adicionado arquivos base do sistema de estoque

---
+++ CRUD_ESTOQUE/listar_produto.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>MyMei - Produtos</title>
+  </head>
+  <body>
+    <h1>Lista de Produtos</h1>
+    <table style="border: 1px solid black; border-collapse: collapse">
+      <thead>
+        <tr>
+          <th style="padding: 8px; border: 1px solid black">Nome</th>
+          <th style="padding: 8px; border: 1px solid black">Preço</th>
+          <th style="padding: 8px; border: 1px solid black">Quantidade</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td style="padding: 8px; border: 1px solid black">Nome do produto</td>
+          <td style="padding: 8px; border: 1px solid black">Preço do produto</td>
+          <td style="padding: 8px; border: 1px solid black">Qtd...</td>
+        </tr>
+      </tbody>
+    </table>
+  </body>
+</html>
